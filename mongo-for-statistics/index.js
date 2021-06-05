455d54cd229407732bebc2254fce8863f03dee0d
HipsterSantos <santoscampos269@gmail.com>
2021-06-05 12:42:59 +0100

    setting up appolo server

--- mongo-for-statistics/index.js
+++ mongo-for-statistics/index.js
@@ -7,25 +7,6 @@ mongoose.connect('mongodb://localhost/tssss',{useNewUrlParser:true,useUnifiedTop
 .then(_=>console.log('listening on port 4000'))
 .catch(console.error);
 
-const useSchema = new Schema({
-  name:{type: String, required:true},
-  age:Number,
-  isMember:Boolean
-})
-const productSchema = new Schema({
-  name:String,
-  price: Number,
-  amount:Number,
-  total:{type:Number,default: function(){return this.price * this.amount}}
-})
-const clientSchema = new Schema({
-  user:{type: Schema.Types.ObjectID,ref:'Users'},
-  whatBought:[{type: productSchema}],
-  refcode:{type:String,default: Math.random() * 100},
-  stats:{
-    totalProducts: {type:Number,default:function(){return this.whatBought.length}}
-  }
-})
 app.get('/',async(req,res)=>{
   console.log(`Hey I'm listening on port 4000`);
 })
