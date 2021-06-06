ee85c3f64000d318c69ee4d7de0a4900da319574
HipsterSantos <santoscampos269@gmail.com>
2021-06-06 03:47:14 +0100

    tweaking apollo server for

--- chat-app/sample.js
+++ chat-app/sample.js
@@ -8,15 +8,24 @@ const schema = buildSchema (`
 
  type Query{ 
     users: [User!]!
-    message: String
+    user(id:ID!): User
+    messages: [Message!]!
   }
   type User{
       id: ID!
       email: String! 
       name: String! 
       avatarUrl: String!
+      messages:Message
   }
-
+  
+  type Message{
+      id: ID! 
+      body: String!
+      createdAt: String
+      userId:Int
+  }
+  
   type Mutation{
       addUser(email:String!,name: String):User
   }
@@ -37,15 +46,44 @@ const user  ={users:[{
     name:'jessica',
     avatarUrl:'homem'
 }
+],messages:[
+    {
+        id:crypto.randomBytes(40).toString(),userId: 1,
+        body:'Something really '+crypto.randomBytes(20).toString(),
+        createdAt: Date.now()
+    }, 
+    {
+        id:crypto.randomBytes(40).toString(),userId: 1,
+        body:'Something really '+crypto.randomBytes(20).toString(),
+        createdAt: Date.now()
+    }, 
+    {
+        id:crypto.randomBytes(40).toString(),userId: 1,
+        body:'Something really '+crypto.randomBytes(20).toString(),
+        createdAt: Date.now()
+    }, 
+    
 ]
 };
+class User{
+    constructor(user){
+        Object.assign(this,user);
+    }
+    message(){
+        return user.messages.filter(m=>m.userId === this.id);
+    }
+}
 const rootValue = {
-    message:  ()=> 'GrahQL works',
-    users: ()=>  user.users,
-    addUser:()=>{
-        const user = {
-            
+    messages:  ()=> user.messages,
+    users: ()=>  user.users.map(c=>new User(c).message() ),
+    user: ({id})=>user.users.filter(vl => vl.id == id),
+    addUser:({email,name})=>{
+        const newUser = {
+            id: crypto.randomBytes(10).toString(),
+            name,email
         }
+        user.users.push(newUser)
+        return newUser;
     }
 }
 
@@ -54,9 +92,11 @@ graphql(
     `
     query{
         users{
-            name
-            id
             email
+            messages{
+                id
+                body
+            }
         }
     }
     ` ,
