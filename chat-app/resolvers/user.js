af977619c05b5fabe946cc619ef8f09ab9c92b5e
HipsterSantos <santoscampos269@gmail.com>
2021-06-06 11:06:13 +0100

    updating'< :

--- chat-app/resolvers/user.js
+++ chat-app/resolvers/user.js
@@ -1,11 +1,22 @@
+const { mongoose } = require('mongoose');
+const { UserInputError } = require('apollo-server-express');
+const User = require('../models/user');
+
 module.exports = {
 Query: { 
-    users: (root,args, context,info)=>{},
-    user: (root,args,context,info) =>{}
+    users: (root,args, context,info)=>{
+        return User.find({})
+    },
+    user: (root,{id},context,info) =>{
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            throw new UserInputError(`${id} is not valid id`)
+        }
+        return User.findById(id)
+    }
 },
 Mutation:{
     signUp:(root,args,context,info)=> {
-
+        return User.create(args)
     }
 }
 
